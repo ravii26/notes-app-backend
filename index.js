3460de5db47e3344b4ca24af7942b4f631eda424
Ravindra Zala <ravindra@kriyaninfotech.com>
2025-04-03 12:34:55 +0530

    refactor: simplify CORS configuration in index.js to allow all origins

--- index.js
+++ index.js
@@ -30,18 +30,7 @@ import deviceRoutes from "./routes/deviceRoutes.js";
 import jwt from "jsonwebtoken";
 import { errorHandler } from "./middleware/errorHandler.js";
 
-app.use(cors());
-// app.use(
-//   cors({
-//     origin:
-//       process.env.NODE_ENV === "production"
-//         ? process.env.ALLOWED_ORIGINS.split(",")
-//         : "http://localhost:3000",
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
+app.use(cors({ origin: '*' }));
 app.use(express.json({ limit: "10mb" }));
 app.use(bp.json());
 dotenv.config();
