b67807f1fb781a2fb8e4fdc2a7f3f3ed226056ca
Ravindra Zala <ravindra@kriyaninfotech.com>
2025-06-27 15:25:11 +0530

    fix: remove trailing slash from CORS origin URL

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "https://notes-app-frontend-1qa8.vercel.app/",
+      "https://notes-app-frontend-1qa8.vercel.app",
       "http://localhost:3000",
     ],
   },
